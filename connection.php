9bfbb577bfa6bc1d568baebaee8800ea85321f77
smnayan <31862252+smnayan@users.noreply.github.com>
2019-09-10 22:09:36 +0600

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+ 
+ $servername = "localhost";
+ $username = "SultanMahmud";
+ $password = "01843132099";
+ $dbname = "social_network";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+?>
